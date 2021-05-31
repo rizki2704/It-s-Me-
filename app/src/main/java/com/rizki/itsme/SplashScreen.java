e3653e90ff64a80ec8b505b763869f3792a26569
resturr <restur2411@gmail.com>
2021-05-31 14:22:59 +0700

    Walkthrough + Logic

--- app/src/main/java/com/rizki/itsme/SplashScreen.java
+++ app/src/main/java/com/rizki/itsme/SplashScreen.java
@@ -12,7 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 public class SplashScreen extends AppCompatActivity {
-
+// 28 Mei 2021, 10118025 Rizki Restu Illahi IF-1
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
