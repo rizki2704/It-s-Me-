0454deb8b9f79a8f999f61b1bae25331a6d1cb21
resturr <restur2411@gmail.com>
2021-05-31 21:09:15 +0700

    Bottom Navigation

--- app/src/main/java/com/rizki/itsme/MainScreen.java
+++ app/src/main/java/com/rizki/itsme/MainScreen.java
@@ -1,14 +1,74 @@
 package com.rizki.itsme;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+import com.rizki.itsme.fragment.ActivityFragment;
+import com.rizki.itsme.fragment.GalleryFragment;
+import com.rizki.itsme.fragment.HomeFragment;
+import com.rizki.itsme.fragment.MusicFragment;
+import com.rizki.itsme.fragment.ProfileFragment;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
+
 public class MainScreen extends AppCompatActivity {
+
+    private MeowBottomNavigation bnv_Main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        bnv_Main = findViewById(R.id.bnv_Main);
+        bnv_Main.add(new MeowBottomNavigation.Model(1,R.drawable.ic_home));
+        bnv_Main.add(new MeowBottomNavigation.Model(2,R.drawable.ic_music));
+        bnv_Main.add(new MeowBottomNavigation.Model(3,R.drawable.ic_profile));
+        bnv_Main.add(new MeowBottomNavigation.Model(4,R.drawable.running));
+        bnv_Main.add(new MeowBottomNavigation.Model(5,R.drawable.picture));
+
+        bnv_Main.show(1,true);
+        replace(new HomeFragment());
+        bnv_Main.setOnClickMenuListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+            @Override
+            public Unit invoke(MeowBottomNavigation.Model model) {
+                switch (model.getId()){
+                    case 1:
+                        replace(new HomeFragment());
+                        break;
+
+                    case 2:
+                        replace(new MusicFragment());
+                        break;
+
+                    case 3:
+                        replace(new ProfileFragment());
+                        break;
+                    case 4:
+                        replace(new ActivityFragment());
+                        break;
+                    case 5:
+                        replace(new GalleryFragment());
+                        break;
+
+                }
+                return null;
+            }
+        });
+
+
+
+
+    }
+
+    private void replace(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame,fragment);
+        transaction.commit();
     }
 }
